8fafbe247f3dc9b09814f4c8369b05c192519dba
Prathyusha-633 <79082086+Prathyusha-633@users.noreply.github.com>
2021-07-19 00:04:37 +0530

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+	<title>Menu</title>
+	<style>
+	body{
+		overflow:hidden;
+		font-style:serif;
+		background-color:yellow;
+		background-size: cover;
+  background-image:url("bgme.jpg");
+  height: 100%; 
+  
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+	}
+	
+	a {
+  color: white;
+  text-decoration: none;
+  
+}
+nav a:hover{
+    text-decoration:none;
+    color:inherit;
+}
+nav a:link{
+	text-decoration:none
+	color: black;
+}
+.an1,.an2,.an3
+{
+	
+	
+	
+}
+.menu
+{
+	background-color:white;
+	margin-top:none;
+	margin-bottom:330px;
+	background-color:yellow;
+	background-size: cover;
+  background-image:url("bgme.jpg");
+  height: 100%; 
+  
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.ani{
+	margin-left:20px;
+}
+.t{
+	margin-top:0px;
+}
+		
+		
+	
+	
+	
+	</style>
+	
+</head>
+<body>
+	<div class="menu">	
+		<table border="0"  class="t" width="178px" height="150px;" >
+			<tr><td><a href="main.html" target="_top"><img src="prologo.png" alt="logo" width="190" height="100"></a></td></tr>
+			<tr><td style="background-color:#f71b14;border:1px solid black;text-align:center;padding: 10px 0px;"><a  class="an1" href="donate.php" target = "content"><h3>DONATE</h3></a></br></td></tr>
+			<tr><td  style="background-color:#f71b14;border:1px solid black;text-align:center;padding:10px 0px;"><a  class="an2" href="receive.php" target = "content"><h3>RECEIVE</h3></a></br></td></tr>
+			
+			
+		 
+			<tr><td  style="background-color:#f71b14;border:1px solid black;text-align:center;padding:10px 0px"><a class="an3" href="bgav.php" target="content"><h3>AVAILABLE</h3></a></td></tr></br></br></br></br>
+			<tr><td><a class="ani" href="Static.html" target="_top"  ><img src="lgot.png" width="130" height="130"></a>
+		</table>	
+		
+		</div>
+		</body>
+
+	
+
+
+</html>
